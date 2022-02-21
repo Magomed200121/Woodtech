051ce6d923348e888fb93299b295931638e77379
Magomed200121 <89939948+Magomed200121@users.noreply.github.com>
2022-02-21 18:01:40 +0300

    create

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+
+// Variables
+const burger = document.querySelector('.burger')
+const topMenu = document.querySelector('.top-menu')
+const homeSlider = new Swiper('.home-slider', {
+    speed: 800,
+    effect: 'fade',
+    centerSlides: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 4000,
+        
+    },
+})
+
+// Event Listeners
+burger.addEventListener('click', toggleBurger)
+
+
+// Functions
+function toggleBurger() {
+    burger.classList.toggle('burger_active')
+    toggleMenu()
+}
+
+function toggleMenu() {
+    topMenu.classList.toggle('menulist_active')
+}
